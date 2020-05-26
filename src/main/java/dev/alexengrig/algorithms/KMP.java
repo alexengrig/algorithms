427878f02f3c4d1c935cebfe4f00727b12b747fa
Grig Alex <alexengrigdev@gmail.com>
2020-05-27 00:25:22 +0300

    KMP: Create all and nonOverlap methods

--- src/main/java/dev/alexengrig/algorithms/KMP.java
+++ src/main/java/dev/alexengrig/algorithms/KMP.java
@@ -1,5 +1,8 @@
 package dev.alexengrig.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KMP {
     public static int first(String pattern, String text) {
         PrefixFunction prefixFunction = new PrefixFunction(pattern);
@@ -15,6 +18,42 @@ public class KMP {
             }
         }
         return -1;
+    }
+
+    public static int[] all(String pattern, String text) {
+        PrefixFunction prefixFunction = new PrefixFunction(pattern);
+        List<Integer> target = new ArrayList<>();
+        for (int i = 0, p = pattern.length(), l = text.length() - p + 1; i < l; ) {
+            String part = text.substring(i, i + p);
+            int length = matchLength(pattern, part);
+            if (length == p) {
+                target.add(i);
+            }
+            if (length > 0) {
+                i += length - prefixFunction.lengthAt(length - 1);
+            } else {
+                i++;
+            }
+        }
+        return target.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static int[] nonOverlap(String pattern, String text) {
+        PrefixFunction prefixFunction = new PrefixFunction(pattern);
+        List<Integer> target = new ArrayList<>();
+        for (int i = 0, p = pattern.length(), l = text.length() - p + 1; i < l; ) {
+            String part = text.substring(i, i + p);
+            int length = matchLength(pattern, part);
+            if (length == p) {
+                target.add(i);
+                i += length;
+            } else if (length > 0) {
+                i += length - prefixFunction.lengthAt(length - 1);
+            } else {
+                i++;
+            }
+        }
+        return target.stream().mapToInt(Integer::intValue).toArray();
     }
 
     private static int matchLength(String pattern, String text) {
--- src/test/java/dev/alexengrig/algorithms/KMPTest.java
+++ src/test/java/dev/alexengrig/algorithms/KMPTest.java
@@ -2,11 +2,24 @@ package dev.alexengrig.algorithms;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class KMPTest {
     @Test
-    public void test() {
+    public void testFirst() {
         assertEquals(1, KMP.first("aba", "baba"));
+    }
+
+    @Test
+    public void testAll() {
+        assertArrayEquals(new int[]{0, 4}, KMP.all("ABA", "ABACABAD"));
+        assertArrayEquals(new int[]{0, 2, 4}, KMP.all("ABA", "ABABABA"));
+    }
+
+    @Test
+    public void testNonOverlap() {
+        assertArrayEquals(new int[]{0, 4}, KMP.nonOverlap("ABA", "ABACABAD"));
+        assertArrayEquals(new int[]{0, 4}, KMP.nonOverlap("ABA", "ABABABA"));
     }
 }
