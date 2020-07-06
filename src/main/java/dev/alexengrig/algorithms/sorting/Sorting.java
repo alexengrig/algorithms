2578e1410ad4558c6aae9ce017d6552db0a0be73
Grig Alex <alexengrigdev@gmail.com>
2020-07-06 22:24:31 +0300

    Sorting: Fix Sorting#swap

--- src/main/java/dev/alexengrig/algorithms/sorting/Sorting.java
+++ src/main/java/dev/alexengrig/algorithms/sorting/Sorting.java
@@ -2,8 +2,10 @@ package dev.alexengrig.algorithms.sorting;
 
 public class Sorting {
     public static void swap(int[] array, int from, int to) {
-        array[from] = array[from] + array[to];
-        array[to] = array[from] - array[to];
-        array[from] = array[from] - array[to];
+        if (from != to) {
+            array[from] = array[from] + array[to];
+            array[to] = array[from] - array[to];
+            array[from] = array[from] - array[to];
+        }
     }
 }
--- src/test/java/dev/alexengrig/algorithms/sorting/SortingTest.java
+++ src/test/java/dev/alexengrig/algorithms/sorting/SortingTest.java
@@ -7,10 +7,12 @@ import static org.junit.Assert.assertArrayEquals;
 public class SortingTest {
     @Test
     public void should_swap() {
-        int[] array = {2, 1};
+        int[] array = {2, 1, 1};
         Sorting.swap(array, 0, 1);
-        assertArrayEquals(new int[]{1, 2}, array);
+        assertArrayEquals(new int[]{1, 2, 1}, array);
         Sorting.swap(array, 0, 1);
-        assertArrayEquals(new int[]{2, 1}, array);
+        assertArrayEquals(new int[]{2, 1, 1}, array);
+        Sorting.swap(array, 1, 2);
+        assertArrayEquals(new int[]{2, 1, 1}, array);
     }
 }
