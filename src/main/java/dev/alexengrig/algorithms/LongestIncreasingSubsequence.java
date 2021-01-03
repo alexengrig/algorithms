133860cd8873d3ecedc07d598aa1a3baabee5123
Grig Alex <alexengrigdev@gmail.com>
2021-01-03 14:38:56 +0300

    LIS: Draft

--- src/main/java/dev/alexengrig/algorithms/LongestIncreasingSubsequence.java
+++ src/main/java/dev/alexengrig/algorithms/LongestIncreasingSubsequence.java
@@ -1,11 +1,16 @@
 package dev.alexengrig.algorithms;
 
+import java.util.Arrays;
+
 public class LongestIncreasingSubsequence {
     public static int length(int[] array) {
         return length(array, array.length);
     }
 
     public static int length(int[] array, int length) {
+        if (length == 0) {
+            return 0;
+        }
         int[] lis = new int[length];
         int max = 0;
         for (int i = 1; i < length; i++) {
@@ -19,6 +24,39 @@ public class LongestIncreasingSubsequence {
             }
         }
         return max + 1;
+    }
+
+    public static int[] values(int[] array) {
+        return values(array, array.length);
+    }
+
+    public static int[] values(int[] array, int length) {
+        if (length <= 1) {
+            return Arrays.copyOf(array, length);
+        }
+        int[] lis = new int[length];
+        int[] target = new int[0];
+        for (int i = 1; i < length; i++) {
+            int[] arr = new int[i];
+            int k = 0;
+            for (int j = 0; j < i; j++) {
+                if (array[i] > array[j] && lis[i] < lis[j] + 1) {
+                    arr[k++] = array[j];
+                    lis[i] = lis[j] + 1;
+                }
+            }
+            if (k < i) {
+                arr[k++] = array[i];
+            }
+            if (k > target.length) {
+                target = Arrays.copyOf(arr, k);
+            }
+        }
+        return target;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Target: " + Arrays.toString(values(new int[]{1, 10, 2, 9, 3, 8, 4, 7, 5, 6})));
     }
 
     public static class Recursive {
--- src/test/java/dev/alexengrig/algorithms/LongestIncreasingSubsequenceTest.java
+++ src/test/java/dev/alexengrig/algorithms/LongestIncreasingSubsequenceTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static dev.alexengrig.algorithms.LongestIncreasingSubsequence.Recursive;
 import static dev.alexengrig.algorithms.LongestIncreasingSubsequence.length;
+import static dev.alexengrig.algorithms.LongestIncreasingSubsequence.values;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class LongestIncreasingSubsequenceTest {
@@ -15,6 +17,14 @@ public class LongestIncreasingSubsequenceTest {
         assertEquals(5, length(new int[]{1, 2, 3, 4, 5}));
         assertEquals(5, length(new int[]{1, 10, 2, 9, 3, 8, 4, 7, 5}));
         assertEquals(6, length(new int[]{1, 6, 2, 7, 3, 8, 4, 9, 5, 10}));
+    }
+
+    @Test
+    public void should_return_values() {
+//        assertArrayEquals(new int[]{}, values(new int[]{}));
+//        assertArrayEquals(new int[]{1}, values(new int[]{1}));
+//        assertArrayEquals(new int[]{1, 2}, values(new int[]{1, 2}));
+        assertArrayEquals(new int[]{1}, values(new int[]{1, 0, -1, -2}));
     }
 
     @Test
